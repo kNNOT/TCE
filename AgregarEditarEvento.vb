39f1a5d2f974086868b1371d3d036f2190e61a69
Alex <alexuymw3@hotmail.com>
2018-09-16 03:44:40 -0500

    add and fix

--- AgregarEditarEvento.vb
+++ AgregarEditarEvento.vb
@@ -7,6 +7,7 @@ Public Class addorEditEvent
 
     Public Sub New(ByVal uMod As Boolean)
         InitializeComponent()
+        Me.uMod = uMod
         If uMod = False Then
             cbSlcEventToEdit.Visible = False
         Else
@@ -27,10 +28,57 @@ Public Class addorEditEvent
         iDB.returnData("SELECT idEvents, name_events FROM Events", cbSlcEventToEdit)
     End Sub
 
+    Private Sub addoreditEvent(sender As Object, e As EventArgs) Handles btnAddEvent.Click
+        If TBoxEventName.Text = String.Empty Then
+            MessageBox.Show("Falta el nombre del evento", "Campo vacío", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        If TBoxCityEvent.Text = String.Empty Then
+            MessageBox.Show("Falta la ciudad donde se hará evento", "Campo vacío", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        If TBoxEventDirec.Text = String.Empty Then
+            MessageBox.Show("Falta la dirección del evento", "Campo vacío", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        If mTBoxStartEventDate.Text = defaultmask Then
+            MessageBox.Show("Falta la fecha de inicio del evento", "Campo vacío", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        If mTBoxEndEventDate.Text = defaultmask Then
+            MessageBox.Show("Falta la fecha final del evento", "Campo vacío", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        If TBoxEventPrice.Text = String.Empty Then
+            MessageBox.Show("Falta el precio del evento", "Campo vacío", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim fechaInicial As Date = mTBoxStartEventDate.Text
+        Dim fechaFinal As Date = mTBoxEndEventDate.Text
+
+        If fechaInicial > fechaFinal Then
+            MessageBox.Show("La fecha inicial del evento no puede ser mayor a la fecha final.", "Orden de fechas incorrectas", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            mTBoxStartEventDate.Select()
+            Return
+        End If
+
+        If uMod = False Then
+            addEvent()
+        Else
+            editEvent()
+        End If
+    End Sub
+
     Private Sub addEvent()
         If iDB.Query($"INSERT INTO Events(name_events,city,direcc,datei,datef,stages,minimumAge,ticketsopen,priceEvent)
                      VALUES('{TBoxEventName.Text}', '{TBoxCityEvent.Text}', '{TBoxEventDirec.Text}', '{mTBoxStartEventDate.Text}', 
-                     {mTBoxEndEventDate.Text}, {nudStagesNumbers.Value}, {nudMinimumAge.Value}, {nudCtnEntradas.Value}, {TBoxEventPrice.Text})") = True Then
+                     '{mTBoxEndEventDate.Text}', {nudStagesNumbers.Value}, {nudMinimumAge.Value}, {nudCtnEntradas.Value}, {TBoxEventPrice.Text})") = True Then
             MessageBox.Show("Se ha agregado el evento.", "¡Hecho!", MessageBoxButtons.OK, MessageBoxIcon.Information)
         End If
     End Sub
@@ -40,6 +88,7 @@ Public Class addorEditEvent
                      datei='{mTBoxStartEventDate.Text}', datef='{mTBoxEndEventDate.Text}', stages={nudStagesNumbers.Value}, minimumAge={nudMinimumAge.Value}, ticketsopen={nudCtnEntradas.Value}, priceEvent={TBoxEventPrice.Text}
                       WHERE idEvents={idEvent}") = True Then
             MessageBox.Show("Se ha editado el evento.", "¡Hecho!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            cbSlcEventToEdit.SelectedIndex = 0
         End If
     End Sub
 
@@ -57,16 +106,41 @@ Public Class addorEditEvent
             nudStagesNumbers.Value = 1
         Else
             layoutAddNewEvent.Enabled = True
-            idEvent = returnID(cbSlcEventToEdit.SelectedItem.ToString)
-            TBoxEventName.Text = iDB.returnData($"SELECT name_events FROM Events WHERE idEvents={idEvent}")
-            TBoxCityEvent.Text = iDB.returnData($"SELECT city FROM Events WHERE idEvents={idEvent}")
-            TBoxEventDirec.Text = iDB.returnData($"SELECT direcc FROM Events WHERE idEvents={idEvent}")
-            TBoxEventPrice.Text = iDB.returnData($"SELECT priceEvent FROM Events WHERE idEvents={idEvent}")
-            mTBoxStartEventDate.Text = iDB.returnData($"SELECT datei FROM Events WHERE idEvents={idEvent}")
-            mTBoxEndEventDate.Text = iDB.returnData($"SELECT datef FROM Events WHERE idEvents={idEvent}")
-            nudCtnEntradas.Value = iDB.returnData($"SELECT ticketsopen FROM Events WHERE idEvents={idEvent}")
-            nudMinimumAge.Value = iDB.returnData($"SELECT minimumAge FROM Events WHERE idEvents={idEvent}")
-            nudStagesNumbers.Value = iDB.returnData($"SELECT stages FROM Events WHERE idEvents={idEvent}")
+            idEvent = returnID(cbSlcEventToEdit.SelectedItem.ToString) 'Obtiene el ID del item seleccionado en el combobox
+            'Llenar cada TextBox, NumericUpDown y MaskedTextBox con valores de la base de datos segun el evento seleccionado.
+            TBoxEventName.Text = iDB.returnData($"SELECT name_events FROM Events WHERE idEvents={idEvent}") 'Nombre del evento
+            TBoxCityEvent.Text = iDB.returnData($"SELECT city FROM Events WHERE idEvents={idEvent}") 'Ciudad del evento
+            TBoxEventDirec.Text = iDB.returnData($"SELECT direcc FROM Events WHERE idEvents={idEvent}") 'Direccion del evento
+            TBoxEventPrice.Text = iDB.returnData($"SELECT priceEvent FROM Events WHERE idEvents={idEvent}") 'Precio del evento
+            mTBoxStartEventDate.Text = iDB.returnData($"SELECT datei FROM Events WHERE idEvents={idEvent}") 'Fecha de inicio del evento
+            mTBoxEndEventDate.Text = iDB.returnData($"SELECT datef FROM Events WHERE idEvents={idEvent}") 'Fecha final del evento
+            nudCtnEntradas.Value = iDB.returnData($"SELECT ticketsopen FROM Events WHERE idEvents={idEvent}") 'Cantidad de entradas disponibles
+            nudMinimumAge.Value = iDB.returnData($"SELECT minimumAge FROM Events WHERE idEvents={idEvent}") 'Edad minima para asistir al evento
+            nudStagesNumbers.Value = iDB.returnData($"SELECT stages FROM Events WHERE idEvents={idEvent}") 'Cantidad de palcos.
         End If
+    End Sub
+
+    Private Sub StartEndEventDateTVC(sender As Object, e As TypeValidationEventArgs) Handles mTBoxStartEventDate.TypeValidationCompleted, mTBoxEndEventDate.TypeValidationCompleted
+        If sender.Equals(mTBoxStartEventDate) And Not e.IsValidInput Then
+            MessageBox.Show("La fecha no es válida.", "Fecha inválida", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            mTBoxStartEventDate.Select()
+            Return
+        ElseIf sender.Equals(mTBoxEndEventDate) And Not e.IsValidInput Then
+            MessageBox.Show("La fecha no es válida.", "Fecha inválida", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            mTBoxEndEventDate.Select()
+            Return
+        End If
+    End Sub
+
+    Private Sub btnHelpClic(sender As Object, e As EventArgs) Handles btnHelp.Click
+        MessageBox.Show($"Llene todos los campos vacíos para agregar un nuevo evento. {vbLf}Seleccione un elemento en la lista (si ésta aparece) para modificar un evento.", "Mensaje de ayuda", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub btnHelpME(sender As Object, e As EventArgs) Handles btnHelp.MouseEnter
+        btnHelp.BackColor = Color.FromArgb(160, 160, 160)
+    End Sub
+
+    Private Sub btnHelpML(sender As Object, e As EventArgs) Handles btnHelp.MouseLeave
+        btnHelp.BackColor = Me.BackColor
     End Sub
 End Class
--- DBConnection.vb
+++ DBConnection.vb
@@ -9,7 +9,7 @@ Public Class DBConnection
     'Método que devuelve los datos de la conexion
     Private Function dConexion() As MySqlConnectionStringBuilder
         datos = New MySqlConnectionStringBuilder
-        datos.Server = "thecodeeye.sytes.net"
+        datos.Server = "192.168.1.225" 'IP: thecodeeye.sytes.net
         datos.Port = 3306
         datos.UserID = "alexuy"
         datos.Password = "416C65785F31113A"
@@ -18,11 +18,11 @@ Public Class DBConnection
     End Function
 
     'Método que devuelve la conexion ya abierta.
-    Private Async Function Conexion() As Task(Of MySqlConnection)
+    Private Function Conexion() As MySqlConnection
         con = New MySqlConnection(dConexion().ToString)
         Try
             If con.State = ConnectionState.Closed Then
-                Await con.OpenAsync()
+                con.Open()
             End If
         Catch e As MySqlException
             MessageBox.Show("Error al conectar con la base de datos", "Error de conexion", MessageBoxButtons.OK, MessageBoxIcon.Error)
@@ -31,9 +31,9 @@ Public Class DBConnection
     End Function
 
     'Método que ejecuta los comandos: INSERT, UPDATE y DELETE.
-    Public Function Query(ByVal qry As String) As Boolean
+    Public Function Query(qry As String) As Boolean
         Try
-            cmd = New MySqlCommand(qry, Conexion().Result)
+            cmd = New MySqlCommand(qry, Conexion())
             cmd.ExecuteNonQuery()
             Return True
         Catch ex As MySqlException
@@ -49,7 +49,7 @@ Public Class DBConnection
     'Es necesario utilizar solo una columna y un WHERE, o sino; devolverá solo el elemento de la primera columna y fila (0, 0).
     Public Function returnData(qry As String) As String
         Try
-            cmd = New MySqlCommand(qry, Conexion().Result)
+            cmd = New MySqlCommand(qry, Conexion())
             reader = cmd.ExecuteReader()
             reader.Read()
             Return reader.GetString(0)
@@ -64,11 +64,11 @@ Public Class DBConnection
 
     'Método que llena todo un combobox con elementos, estos elementos son el ID (la llave primaria de las tablas de la base de datos)
     'y el nombre correspondiendo (evento o grupo).
-    Public Async Sub returnData(qry As String, cb As ComboBox)
+    Public Sub returnData(qry As String, cb As ComboBox)
         Try
-            cmd = New MySqlCommand(qry, Conexion().Result)
+            cmd = New MySqlCommand(qry, Conexion())
             reader = cmd.ExecuteReader()
-            While Await reader.ReadAsync() 'si hay alguna fila para leer, devuelve True (entra al código del While); si no hay, devuelve False y no entra al código del While.
+            While reader.Read() 'si hay alguna fila para leer, devuelve True (entra al código del While); si no hay, devuelve False y no entra al código del While.
                 cb.Items.Add(reader.GetString(0) & ". " & reader.GetString(1))
             End While
         Catch ex As MySqlException
@@ -81,12 +81,12 @@ Public Class DBConnection
     End Sub
 
     'Método que llena todo un datagridview con elemntos, estos elementos comunmente son todas las columnas y filas de la tabla necesaria.
-    Public Async Sub returnData(qry As String, dgv As DataGridView, clmnCount As Integer)
+    Public Sub returnData(qry As String, dgv As DataGridView, clmnCount As Integer)
         Try
-            cmd = New MySqlCommand(qry, Conexion().Result)
+            cmd = New MySqlCommand(qry, Conexion())
             reader = cmd.ExecuteReader()
             dgv.Rows.Clear() 'Limpia todas las filas que hay en el datagridview
-            While Await reader.ReadAsync() 'si hay alguna fila para leer, devuelve True (entra al código del While); si no hay, devuelve False y no entra al código del While.
+            While reader.Read() 'si hay alguna fila para leer, devuelve True (entra al código del While); si no hay, devuelve False y no entra al código del While.
                 If clmnCount = 10 Then 'Numero de columnas de la ventana StartMenu
                     dgv.Rows.Add(reader.GetString(0), reader.GetString(1), $"{reader.GetString(2)}, {reader.GetString(3)}", $"{reader.GetString(4)} - {reader.GetString(5)}",
                                             reader.GetString(6), reader.GetString(7), reader.GetString(8), reader.GetString(9))
